cca0269a646648ea0a770c084739858a8ed8cae3
Jorgeur710 <37023175+Jorgeur710@users.noreply.github.com>
2024-12-16 18:40:50 +0100

    Definitivo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = reqire('cors');
 const port = process.env.PORT || 3000;
 const routes = require('./v1/routes/tenis_routes');
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 app.use("/api/v1/partidos",routes);
 
 app.listen(port,()=> console.log(`listening on http://localhost:${port}`))
