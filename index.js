bfa2a3a0ffaf2aca80f76328da1d0a5c17071b7d
Jorgeur710 <37023175+Jorgeur710@users.noreply.github.com>
2024-12-17 12:24:50 +0100

    Definitivo v3

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const port = process.env.PORT || 3000;
 const routes = require('./v1/routes/tenis_routes');
 
 const app = express();
-app.use(express.json())
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+app.use(express.json());
 app.use(cors());
+
 app.use("/api/v1/partidos",routes);
 
 app.listen(port,()=> console.log(`listening on http://localhost:${port}`))
